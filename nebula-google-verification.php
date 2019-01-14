8b71aacb32d90e57bb60554a594507bab3f772a8
Katrine-Marie <fjordscene@gmail.com>
2019-01-14 14:10:22 +0100

    Base setup

---
+++ nebula-google-verification.php
@@ -0,0 +1,17 @@
+<?php
+/**********
+* Plugin Name: Nebula Google Verification
+* Plugin URI: http://google.com
+* Description: Verify your site with Google Search Console by inserting the google meta tag into your site's head section.
+* Version: 1.0.0
+* Author: Katrine-Marie Burmeister
+* Author URI: https://fjordstudio.dk
+* License: GPL-2.0+
+License URI: http://www.gnu.org/licenses/gpl-2.0.txt
+*/
+
+namespace nebula\console;
+
+if(!defined('ABSPATH')){
+	exit('Go away!');
+}
