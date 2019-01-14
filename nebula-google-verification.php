d7e3cb211f2a76b5a37ee219ef71d81c9627a97a
Katrine-Marie <fjordscene@gmail.com>
2019-01-14 14:12:19 +0100

    Added initialization class

--- nebula-google-verification.php
+++ nebula-google-verification.php
@@ -15,3 +15,25 @@ namespace nebula\console;
 if(!defined('ABSPATH')){
 	exit('Go away!');
 }
+
+define('nebula_gsearch_DIR', plugin_dir_path(__FILE__));
+$mystart = new MyInit();
+
+// TODO: INCLUDE files and instantiate classes
+
+class MyInit{
+  public function __construct(){
+    register_activation_hook( __FILE__, array($this, 'plugin_activated' ));
+    register_deactivation_hook( __FILE__, array($this, 'plugin_deactivated' ));
+    register_uninstall_hook( __FILE__, array($this, 'plugin_uninstall' ) );
+  }
+  public static function plugin_activated(){
+		set_transient('_gsearch_welcome',true,30);
+  }
+  public function plugin_deactivated(){
+
+  }
+  public function plugin_uninstall() {
+
+  }
+}
